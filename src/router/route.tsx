a6aa1a20f4f5a897953ccc07c7ca72c87ab563e1
mufeiyu <2382839439@qq.com>
2025-04-18 19:42:04 +0800

    feat:init

---
+++ src/router/route.tsx
@@ -0,0 +1,19 @@
+import type { MenuProps } from 'antd'
+import Page1 from '@/views/page1'
+import Page2 from '@/views/page2'
+import { AppstoreOutlined } from '@ant-design/icons'
+
+export const routes = [
+  {
+    label: 'page1',
+    icon: <AppstoreOutlined />,
+    key: '/page1',
+    element: <Page1 />,
+  },
+  {
+    label: 'page2',
+    icon: <AppstoreOutlined />,
+    key: '/page2',
+    element: <Page2 />,
+  },
+]
